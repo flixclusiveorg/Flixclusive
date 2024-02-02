fac72c82d081c1075b72ef37c6132df2c156083a
rhenwinch <rhenwinchester@gmail.com>
2024-02-03 00:24:46 +0800

    fix: proper focus behavior for nav drawer <-> main content (vice versa)

--- core/ui/tv/src/main/kotlin/com/flixclusive/core/ui/tv/util/FocusHelper.kt
+++ core/ui/tv/src/main/kotlin/com/flixclusive/core/ui/tv/util/FocusHelper.kt
@@ -6,11 +6,21 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.focus.FocusRequester
 import com.ramcosta.composedestinations.spec.Route
 
-private val LocalLastFocusedItemPerDestination = compositionLocalOf<MutableMap<String, String>?> { null }
-private val LocalFocusTransferredOnLaunch = compositionLocalOf<MutableState<Boolean>?> { null }
-private val LocalCurrentRoute = compositionLocalOf<String?> { null }
+private val LocalLastFocusedItemPerDestination = compositionLocalOf<MutableMap<String, String>> {
+    error("Please wrap your app with LocalLastFocusedItemPerDestinationProvider")
+}
+private val LocalFocusTransferredOnLaunch = compositionLocalOf<MutableState<Boolean>> {
+    error("Please wrap your app with LocalFocusTransferredOnLaunch")
+}
+private val LocalCurrentRoute = compositionLocalOf<String> {
+    error("Please wrap your app with LocalCurrentRoute")
+}
+private val LocalLastFocusedItemFocusedRequester = compositionLocalOf<MutableState<FocusRequester>> {
+    error("Please wrap your app with LocalLastFocusedItemFocusedRequester")
+}
 
 @Composable
 fun LocalLastFocusedItemPerDestinationProvider(content: @Composable () -> Unit) {
@@ -28,16 +38,22 @@ fun LocalCurrentRouteProvider(currentSelectedScreen: Route, content: @Composable
 }
 
 @Composable
-fun useLocalLastFocusedItemPerDestination(): MutableMap<String, String> {
-    return LocalLastFocusedItemPerDestination.current ?: throw RuntimeException("Please wrap your app with LocalLastFocusedItemPerDestinationProvider")
+fun LocalLastFocusedItemFocusedRequesterProvider(content: @Composable () -> Unit) {
+    CompositionLocalProvider(LocalLastFocusedItemFocusedRequester provides remember { mutableStateOf(FocusRequester()) }, content = content)
 }
 
 @Composable
-fun useLocalCurrentRoute(): String {
-    return LocalCurrentRoute.current ?: throw RuntimeException("Please wrap your app with LocalCurrentRoute")
-}
+fun useLocalLastFocusedItemFocusedRequester()
+    = LocalLastFocusedItemFocusedRequester.current
 
 @Composable
-fun useLocalFocusTransferredOnLaunch(): MutableState<Boolean> {
-    return LocalFocusTransferredOnLaunch.current ?: throw RuntimeException("Please wrap your app with LocalLastFocusedItemPerDestinationProvider")
-}
+fun useLocalLastFocusedItemPerDestination()
+    = LocalLastFocusedItemPerDestination.current
+
+@Composable
+fun useLocalCurrentRoute()
+    = LocalCurrentRoute.current
+
+@Composable
+fun useLocalFocusTransferredOnLaunch()
+    = LocalFocusTransferredOnLaunch.current
