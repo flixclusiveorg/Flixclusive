c0cca7128301453e5e6835055b57d1c6393964d2
rhenwinch <rhenwinchester@gmail.com>
2024-02-03 01:06:01 +0800

    fix: remove tv screens transition horizontal slide

--- app/src/main/kotlin/com/flixclusive/util/NavigationHelper.kt
+++ app/src/main/kotlin/com/flixclusive/util/NavigationHelper.kt
@@ -122,10 +122,10 @@ internal fun AppNavHost(
     DestinationsNavHost(
         engine = rememberAnimatedNavHostEngine(
             rootDefaultAnimations = RootNavGraphDefaultAnimations(
-                enterTransition = { defaultEnterTransition(initialState, targetState) },
-                exitTransition = { defaultExitTransition(initialState, targetState) },
-                popEnterTransition = { defaultPopEnterTransition() },
-                popExitTransition = { defaultPopExitTransition() },
+                enterTransition = { defaultEnterTransition(isTv, initialState, targetState) },
+                exitTransition = { defaultExitTransition(isTv, initialState, targetState) },
+                popEnterTransition = { defaultPopEnterTransition(isTv) },
+                popExitTransition = { defaultPopExitTransition(isTv) },
             )
         ),
         navController = navController,
@@ -160,28 +160,31 @@ private val NavDestination.hostNavGraph: NavGraph
 
 @ExperimentalAnimationApi
 private fun AnimatedContentTransitionScope<*>.defaultEnterTransition(
+    isTv: Boolean,
     initial: NavBackStackEntry,
     target: NavBackStackEntry,
 ): EnterTransition {
     val initialNavGraph = initial.destination.hostNavGraph
     val targetNavGraph = target.destination.hostNavGraph
     // If we're crossing nav graphs (bottom navigation graphs), we crossfade
-    if (initialNavGraph.id != targetNavGraph.id) {
+    if (initialNavGraph.id != targetNavGraph.id || isTv) {
         return fadeIn()
     }
+
     // Otherwise we're in the same nav graph, we can imply a direction
     return fadeIn() + slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start)
 }
 
 @ExperimentalAnimationApi
 private fun AnimatedContentTransitionScope<*>.defaultExitTransition(
+    isTv: Boolean,
     initial: NavBackStackEntry,
     target: NavBackStackEntry,
 ): ExitTransition {
     val initialNavGraph = initial.destination.hostNavGraph
     val targetNavGraph = target.destination.hostNavGraph
     // If we're crossing nav graphs (bottom navigation graphs), we crossfade
-    if (initialNavGraph.id != targetNavGraph.id) {
+    if (initialNavGraph.id != targetNavGraph.id || isTv) {
         return fadeOut()
     }
     // Otherwise we're in the same nav graph, we can imply a direction
@@ -189,11 +192,19 @@ private fun AnimatedContentTransitionScope<*>.defaultExitTransition(
 }
 
 @ExperimentalAnimationApi
-private fun AnimatedContentTransitionScope<*>.defaultPopEnterTransition(): EnterTransition {
+private fun AnimatedContentTransitionScope<*>.defaultPopEnterTransition(isTv: Boolean): EnterTransition {
+    if (isTv) {
+        return fadeIn()
+    }
+
     return fadeIn() + slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.End)
 }
 
 @ExperimentalAnimationApi
-private fun AnimatedContentTransitionScope<*>.defaultPopExitTransition(): ExitTransition {
+private fun AnimatedContentTransitionScope<*>.defaultPopExitTransition(isTv: Boolean): ExitTransition {
+    if (isTv) {
+        return fadeOut()
+    }
+
     return fadeOut() + slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End)
 }
