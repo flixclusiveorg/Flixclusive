b37d34f859646eb6f73df5f7d165f284ab628e99
rhenwinch <rhenwinchester@gmail.com>
2024-02-25 23:50:44 +0800

    fix: FlixHQ sometimes decrypted
    Former-commit-id: 59652f790dd1c51186fc6e657b009b1508479d05

--- core/util/src/main/kotlin/com/flixclusive/core/util/network/JsonHelper.kt
+++ core/util/src/main/kotlin/com/flixclusive/core/util/network/JsonHelper.kt
@@ -1,6 +1,9 @@
 package com.flixclusive.core.util.network
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
 import com.google.gson.reflect.TypeToken
 
 /**
@@ -11,5 +14,37 @@ import com.google.gson.reflect.TypeToken
  * @throws JsonSyntaxException if the JSON is not well-formed or cannot be parsed.
  */
 inline fun <reified T> fromJson(json: String): T {
-    return Gson().fromJson(json, object : TypeToken<T>() {}.type)
+    return Gson()
+        .fromJson(json, object : TypeToken<T>() {}.type)
+}
+
+/**
+ * Parses the specified [JsonElement] into an object of type [T] using Gson library.
+ *
+ * @param json The [JsonElement] to parse.
+ * @return The parsed object of type [T].
+ * @throws JsonSyntaxException if the JSON is not well-formed or cannot be parsed.
+ */
+inline fun <reified T> fromJson(json: JsonElement): T {
+    return Gson()
+        .fromJson(json, object : TypeToken<T>() {}.type)
+}
+
+/**
+ * Parses the specified JSON string into an object of type [T] using Gson library.
+ * With additional parameter to add your custom deserializer.
+ *
+ * @param json The JSON string to parse.
+ * @param serializer The JSON deserializer to use for parsing.
+ * @return The parsed object of type [T].
+ * @throws JsonSyntaxException if the JSON is not well-formed or cannot be parsed.
+ */
+inline fun <reified T> fromJson(
+    json: String,
+    serializer: JsonDeserializer<T>
+): T {
+    return GsonBuilder()
+        .registerTypeAdapter(T::class.java, serializer)
+        .create()
+        .fromJson(json, object : TypeToken<T>() {}.type)
 }
