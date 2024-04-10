8f05522f5f1786c7f0f43fa7f790862469ffaa0b
rhenwinch <rhenwinchester@gmail.com>
2024-04-10 21:53:17 +0800

    fix: ProviderCard icon incorrect placeholder visibility
    Former-commit-id: 2772680b40eb811385b450d98ff81f6597304f2c

--- core/ui/mobile/src/main/kotlin/com/flixclusive/core/ui/mobile/component/ImageWithSmallPlaceholder.kt
+++ core/ui/mobile/src/main/kotlin/com/flixclusive/core/ui/mobile/component/ImageWithSmallPlaceholder.kt
@@ -2,15 +2,22 @@ package com.flixclusive.core.ui.mobile.component
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -18,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import coil.imageLoader
 import coil.request.ImageRequest
 import com.flixclusive.core.ui.common.util.onMediumEmphasis
@@ -31,20 +39,30 @@ fun ImageWithSmallPlaceholder(
     @StringRes contentDescId: Int,
     shape: Shape = CircleShape
 ) {
+    var isSuccess by remember { mutableStateOf(false) }
+
+    val background by animateColorAsState(
+        targetValue = if (isSuccess) Color.Transparent else MaterialTheme.colorScheme.surface,
+        label = "",
+    )
+
     Surface(
         modifier = modifier,
         tonalElevation = 65.dp,
+        color = background,
         shape = shape
     ) {
         Box(
             contentAlignment = Alignment.Center
         ) {
-            Icon(
-                painter = painterResource(id = placeholderId),
-                contentDescription = stringResource(id = contentDescId),
-                tint = LocalContentColor.current.onMediumEmphasis(0.8F),
-                modifier = placeholderModifier
-            )
+            if (!isSuccess) {
+                Icon(
+                    painter = painterResource(id = placeholderId),
+                    contentDescription = stringResource(id = contentDescId),
+                    tint = LocalContentColor.current.onMediumEmphasis(0.8F),
+                    modifier = placeholderModifier
+                )
+            }
 
             if (urlImage != null) {
                 AsyncImage(
@@ -55,6 +73,7 @@ fun ImageWithSmallPlaceholder(
                     imageLoader = LocalContext.current.imageLoader,
                     contentDescription = stringResource(id = contentDescId),
                     contentScale = ContentScale.Crop,
+                    onState = { isSuccess = it is AsyncImagePainter.State.Success },
                     modifier = Modifier
                         .fillMaxSize()
                 )
