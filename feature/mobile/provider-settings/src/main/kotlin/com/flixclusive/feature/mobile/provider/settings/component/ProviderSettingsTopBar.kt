c41e598bf5ca5f05451c9e858d62ba70ef6d3d8c
rhenwinch <rhenwinchester@gmail.com>
2024-04-11 19:37:05 +0800

    add: Change logs dialog with markdown support
    Former-commit-id: f17db97c03f1da5c8da2c9f2ee37f81dca2eea49

--- feature/mobile/provider-settings/src/main/kotlin/com/flixclusive/feature/mobile/provider/settings/component/ProviderSettingsTopBar.kt
+++ feature/mobile/provider-settings/src/main/kotlin/com/flixclusive/feature/mobile/provider/settings/component/ProviderSettingsTopBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -28,14 +29,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.flixclusive.core.util.R
 import com.flixclusive.core.ui.common.R as UiCommonR
+import com.flixclusive.core.util.R as UtilR
 
 @Composable
 internal fun ProviderSettingsTopBar(
     modifier: Modifier = Modifier,
     isVisible: Boolean,
     repositoryUrl: String?,
+    changeLogs: String?,
+    openChangeLogs: () -> Unit,
     onNavigationIconClick: () -> Unit,
 ) {
     val uriHandler = LocalUriHandler.current
@@ -62,12 +65,12 @@ internal fun ProviderSettingsTopBar(
                 IconButton(onClick = onNavigationIconClick) {
                     Icon(
                         painter = painterResource(UiCommonR.drawable.left_arrow),
-                        contentDescription = stringResource(R.string.navigate_up)
+                        contentDescription = stringResource(UtilR.string.navigate_up)
                     )
                 }
 
                 Text(
-                    text = stringResource(id = R.string.provider_settings),
+                    text = stringResource(id = UtilR.string.provider_settings),
                     style = MaterialTheme.typography.bodyLarge.copy(
                         fontWeight = FontWeight.SemiBold
                     ),
@@ -78,13 +81,26 @@ internal fun ProviderSettingsTopBar(
                         .padding(horizontal = 15.dp)
                 )
 
+                changeLogs?.let {
+                    IconButton(
+                        onClick = openChangeLogs
+                    ) {
+                        Icon(
+                            painter = painterResource(UiCommonR.drawable.round_update_24),
+                            contentDescription = stringResource(UtilR.string.provider_updates_log),
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.padding(end = 15.dp))
+                }
+
                 repositoryUrl?.let {
                     IconButton(
                         onClick = { uriHandler.openUri(it) }
                     ) {
                         Icon(
                             painter = painterResource(UiCommonR.drawable.web_browser),
-                            contentDescription = stringResource(R.string.open_web_icon),
+                            contentDescription = stringResource(UtilR.string.open_web_icon),
                         )
                     }
                 }
