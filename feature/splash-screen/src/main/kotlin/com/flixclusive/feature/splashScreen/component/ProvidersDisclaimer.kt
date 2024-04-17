36214301f7ea0e7940364bad6c3539cf11555e7c
rhenwinch <rhenwinchester@gmail.com>
2024-04-17 20:31:05 +0800

    fix: SplashScreen mobile appearance

--- feature/splash-screen/src/main/kotlin/com/flixclusive/feature/splashScreen/component/ProvidersDisclaimer.kt
+++ feature/splash-screen/src/main/kotlin/com/flixclusive/feature/splashScreen/component/ProvidersDisclaimer.kt
@@ -1,14 +1,17 @@
 package com.flixclusive.feature.splashScreen.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.flixclusive.core.util.R as UtilR
 
 @Composable
 fun ProvidersDisclaimer(
+    modifier: Modifier = Modifier,
     understood: (isOptIn: Boolean) -> Unit
 ) {
     Consent(
+        modifier = modifier,
         header = stringResource(id = UtilR.string.provider_disclaimer),
         consentContent = stringResource(id = UtilR.string.disclaimer_provider_message),
         goNext = understood
