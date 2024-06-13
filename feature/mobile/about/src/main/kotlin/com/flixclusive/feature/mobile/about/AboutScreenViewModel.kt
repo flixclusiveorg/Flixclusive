7a76a5261859ab667f0983c4e02ff36211dec396
rhenwinch <rhenwinchester@gmail.com>
2024-06-13 19:36:03 +0800

    feat(about-ui): indicate pre-release status
    Former-commit-id: 5caad9883577b35319089997fbea3ae4d8c5f9e3

--- feature/mobile/about/src/main/kotlin/com/flixclusive/feature/mobile/about/AboutScreenViewModel.kt
+++ feature/mobile/about/src/main/kotlin/com/flixclusive/feature/mobile/about/AboutScreenViewModel.kt
@@ -1,13 +1,26 @@
 package com.flixclusive.feature.mobile.about
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.flixclusive.core.datastore.AppSettingsManager
 import com.flixclusive.data.configuration.AppConfigurationManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class AboutScreenViewModel @Inject constructor(
-    appConfigurationManager: AppConfigurationManager
+    appConfigurationManager: AppConfigurationManager,
+    appSettingsManager: AppSettingsManager
 ) : ViewModel() {
     val currentAppBuild = appConfigurationManager.currentAppBuild
+    val isOnPreRelease = appSettingsManager.appSettings
+        .data.map { it.isUsingPrereleaseUpdates }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = false
+        )
 }
