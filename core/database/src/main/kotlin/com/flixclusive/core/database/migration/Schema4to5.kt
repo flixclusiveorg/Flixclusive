8d29ee8803ce451a6a9714175de27618d3d0f005
rhenwinch <rhenwinchester@gmail.com>
2024-07-03 22:58:41 +0800

    fix(database): fix search history migration

--- core/database/src/main/kotlin/com/flixclusive/core/database/migration/Schema4to5.kt
+++ core/database/src/main/kotlin/com/flixclusive/core/database/migration/Schema4to5.kt
@@ -2,20 +2,21 @@ package com.flixclusive.core.database.migration
 
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import java.util.Date
 
 class Schema4to5 : Migration(4, 5) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        val timeToday = Date().time
-
         db.execSQL("""
             CREATE TABLE IF NOT EXISTS `search_history` (
                 `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-                `ownerId` INTEGER NOT NULL DEFAULT 1, 
+                `ownerId` INTEGER NOT NULL, 
                 `query` TEXT NOT NULL, 
-                `searchedOn` INTEGER NOT NULL DEFAULT $timeToday,
+                `searchedOn` INTEGER NOT NULL,
                 UNIQUE(`query`, `ownerId`)
             )
         """.trimIndent())
+
+        db.execSQL("""
+            CREATE UNIQUE INDEX IF NOT EXISTS `index_search_history_query_ownerId` ON `search_history` (`query`, `ownerId`)
+        """)
     }
 }
