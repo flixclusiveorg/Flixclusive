c411e4a78709588cd1be7ee42dea80a86ca9711a
rhenwinch <rhenwinchester@gmail.com>
2024-07-25 22:27:01 +0800

    feat(provider-test): add provider test screen UI components [not final]

---
+++ model/provider/src/main/kotlin/com/flixclusive/model/provider/ProviderDataExtension.kt
@@ -0,0 +1,26 @@
+package com.flixclusive.model.provider
+
+import com.flixclusive.gradle.entities.ProviderData
+
+/**
+ *
+ * Gets the unique identifier for the provider as
+ * name alone is note reliable enough to determine
+ * the uniqueness of a provider.
+ *
+ * */
+val ProviderData.id: String
+    get() {
+        val repositoryName = if (repositoryUrl != null) {
+            "-" + getRepositoryNameFromUrl(repositoryUrl!!)
+        } else ""
+
+        return "$name$repositoryName-$versionCode"
+    }
+
+private fun getRepositoryNameFromUrl(url: String): String {
+    val regex = "github\\.com/[^/]+/([^/]+)".toRegex()
+    val matchResult = regex.find(url)
+
+    return matchResult?.groups?.get(1)?.value ?: url
+}
