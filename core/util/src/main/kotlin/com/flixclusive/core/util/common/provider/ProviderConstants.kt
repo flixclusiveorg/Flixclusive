2b5b2f8b646594aeb464c0f6d4105b286b7adffa
rhenwinch <rhenwinchester@gmail.com>
2024-08-27 17:54:50 +0800

    chore(provider): clean up formatting and unused code
    Former-commit-id: 4ebcd4e7706387cd6e3a6bb074c0bda3215b5425

--- core/util/src/main/kotlin/com/flixclusive/core/util/common/provider/ProviderConstants.kt
+++
@@ -1,5 +0,0 @@
-package com.flixclusive.core.util.common.provider
-
-import android.os.Environment.getExternalStorageDirectory
-
-val PLUGINS_PATH = getExternalStorageDirectory().absolutePath + "/Flixclusive/repositories/"
