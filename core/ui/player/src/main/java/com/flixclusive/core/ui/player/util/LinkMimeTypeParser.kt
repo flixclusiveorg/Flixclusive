c4c9a6f0de2e10fd26aa3cf7428f06304f98cad4
rhenwinch <rhenwinchester@gmail.com>
2024-08-31 21:14:57 +0800

    feat(player): improve automatic subtitle picker
    Fixes #121
    
    
    Former-commit-id: bfdf095e55b30feb82fb4e3ee82f931df238afb2

--- core/ui/player/src/main/java/com/flixclusive/core/ui/player/util/LinkMimeTypeParser.kt
+++ core/ui/player/src/main/java/com/flixclusive/core/ui/player/util/LinkMimeTypeParser.kt
@@ -1,5 +1,8 @@
 package com.flixclusive.core.ui.player.util
 
+import androidx.media3.common.MimeTypes
+import com.flixclusive.model.provider.Subtitle
+
 object MimeTypeParser {
 
     /**
@@ -15,5 +18,20 @@ object MimeTypeParser {
             || url.contains(".txt")
             || url.contains(".m3u")
             || url.endsWith(".m3u")
+    }
+
+    fun Subtitle.toMimeType(): String {
+        val isLocalSubtitle = url.contains("content://")
+        val uri = if(isLocalSubtitle) {
+            language
+        } else url
+
+        return when {
+            uri.endsWith(".srt", true) || uri.contains(".srt", true) -> MimeTypes.APPLICATION_SUBRIP
+            uri.endsWith(".vtt", true) || uri.contains(".vtt", true) -> MimeTypes.TEXT_VTT
+            uri.endsWith(".ssa", true) || uri.contains(".ssa", true) -> MimeTypes.TEXT_SSA
+            (uri.endsWith(".ttml", true) || uri.contains(".ttml", true)) || (uri.endsWith(".xml", true) || uri.contains(".xml", true)) -> MimeTypes.APPLICATION_TTML
+            else -> MimeTypes.APPLICATION_SUBRIP
+        }
     }
 }
