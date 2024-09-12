9866fea9d87e506d3d58af1c193fda55080b7056
rhenwinch <rhenwinchester@gmail.com>
2024-09-12 22:28:41 +0800

    build(app): prepare for new 2.1.0 pre-release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ plugins {
 
 // Version
 val versionMajor = 2
-val versionMinor = 0
+val versionMinor = 1
 val versionPatch = 0
-val versionBuild = 1
+val versionBuild = 0
 val applicationName: String = libs.versions.applicationName.get()
 val _applicationId: String = libs.versions.applicationId.get()
 val _versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
