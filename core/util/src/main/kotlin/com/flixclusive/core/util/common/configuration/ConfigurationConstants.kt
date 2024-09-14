d6fd3837c2fd3f4523db938b63d358941e9f6a20
rhenwinch <rhenwinchester@gmail.com>
2024-09-14 15:13:42 +0800

    refactor: migrate to new repository owner [part 2]
    Former-commit-id: 82d1d60425ce86736782cb5f49b60884c72d8cf0

--- core/util/src/main/kotlin/com/flixclusive/core/util/common/configuration/ConfigurationConstants.kt
+++ core/util/src/main/kotlin/com/flixclusive/core/util/common/configuration/ConfigurationConstants.kt
@@ -6,6 +6,6 @@ const val GITHUB_LATEST_RELEASE = "$GITHUB_REPOSITORY_URL/releases/latest"
 const val GITHUB_RAW_API_BASE_URL = "https://raw.githubusercontent.com/"
 const val GITHUB_API_BASE_URL = "https://api.github.com/"
 
-const val GITHUB_USERNAME = "rhenwinch"
+const val GITHUB_USERNAME = "flixclusiveorg"
 const val GITHUB_REPOSITORY = "Flixclusive"
 const val GITHUB_CONFIG_REPOSITORY = "flixclusive-config"
