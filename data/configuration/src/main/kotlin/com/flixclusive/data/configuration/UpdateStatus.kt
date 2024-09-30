d8e81c536dd002aaab97a54e6494c591803adbbe
rhenwinch <rhenwinchester@gmail.com>
2024-09-30 23:43:29 +0800

    fix(home): resolve catalogs hot reloading

--- data/configuration/src/main/kotlin/com/flixclusive/data/configuration/UpdateStatus.kt
+++ data/configuration/src/main/kotlin/com/flixclusive/data/configuration/UpdateStatus.kt
@@ -3,10 +3,11 @@ package com.flixclusive.data.configuration
 import com.flixclusive.core.locale.UiText
 
 sealed class UpdateStatus(
-    val errorMessage: UiText? = null
+    val errorMessage: UiText? = null,
+    val updateInfo: AppUpdateInfo? = null,
 ) {
     data object Fetching : UpdateStatus()
-    data class Outdated(val updateInfo: AppUpdateInfo) : UpdateStatus()
-    data class UpToDate(val updateInfo: AppUpdateInfo) : UpdateStatus()
+    class Outdated(updateInfo: AppUpdateInfo) : UpdateStatus(updateInfo = updateInfo)
+    class UpToDate(updateInfo: AppUpdateInfo) : UpdateStatus(updateInfo = updateInfo)
     class Error(errorMessage: UiText?) : UpdateStatus(errorMessage)
 }
