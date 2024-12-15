e6024f9045fe69dd4c3d8f0403105012e591f8d2
rhenwinch <rhenwinchester@gmail.com>
2024-12-15 15:06:48 +0800

    feat(common-ui): add adaptive dp and label styles getter

---
+++ core/ui/common/src/main/kotlin/com/flixclusive/core/ui/common/util/adaptive/AdaptiveStylesUtil.kt
@@ -0,0 +1,46 @@
+package com.flixclusive.core.ui.common.util.adaptive
+
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.flixclusive.core.ui.common.util.onMediumEmphasis
+
+object AdaptiveStylesUtil {
+    @Composable
+    fun getAdaptiveNonEmphasizedLabel(
+        compactFontSize: TextUnit = 14.sp,
+        mediumFontSize: TextUnit = (compactFontSize.value + 4).sp,
+        expandedFontSize: TextUnit = (mediumFontSize.value + 4).sp
+    ): TextStyle {
+        return MaterialTheme.typography.labelLarge.copy(
+            fontWeight = FontWeight.Normal,
+            color = LocalContentColor.current.onMediumEmphasis(),
+            fontSize = AdaptiveUiUtil.getAdaptiveTextUnit(
+                compact = compactFontSize,
+                medium = mediumFontSize,
+                expanded = expandedFontSize
+            )
+        )
+    }
+
+    @Composable
+    fun getAdaptiveSemiEmphasizedLabel(
+        compactFontSize: TextUnit = 14.sp,
+        mediumFontSize: TextUnit = (compactFontSize.value + 4).sp,
+        expandedFontSize: TextUnit = (mediumFontSize.value + 4).sp
+    ): TextStyle {
+        return MaterialTheme.typography.labelLarge.copy(
+            fontWeight = FontWeight.Medium,
+            color = LocalContentColor.current.onMediumEmphasis(0.8F),
+            fontSize = AdaptiveUiUtil.getAdaptiveTextUnit(
+                compact = compactFontSize,
+                medium = mediumFontSize,
+                expanded = expandedFontSize
+            )
+        )
+    }
+}
---
+++ core/ui/common/src/main/kotlin/com/flixclusive/core/ui/common/util/adaptive/AdaptiveUiUtil.kt
@@ -0,0 +1,124 @@
+package com.flixclusive.core.ui.common.util.adaptive
+
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.window.core.layout.WindowHeightSizeClass
+import androidx.window.core.layout.WindowSizeClass
+import androidx.window.core.layout.WindowWidthSizeClass
+
+object AdaptiveUiUtil {
+    val WindowWidthSizeClass.isCompact get() = this == WindowWidthSizeClass.COMPACT
+    val WindowWidthSizeClass.isMedium get() = this == WindowWidthSizeClass.MEDIUM
+    val WindowWidthSizeClass.isExpanded get() = this == WindowWidthSizeClass.EXPANDED
+
+    val WindowHeightSizeClass.isCompact get() = this == WindowHeightSizeClass.COMPACT
+    val WindowHeightSizeClass.isMedium get() = this == WindowHeightSizeClass.MEDIUM
+    val WindowHeightSizeClass.isExpanded get() = this == WindowHeightSizeClass.EXPANDED
+
+
+    /**
+     * A function to get an adaptive DP size unit based on the window size class.
+     *
+     * @param windowSizeClass The current window size class.
+     * @param compact The DP size for a compact window size. Also is the default value if no window size class is provided.
+     * @param medium The DP size for a medium window size.
+     * @param expanded The DP size for an expanded window size.
+     *
+     * @return The DP size for the current window size class.
+     * */
+    @Composable
+    fun getAdaptiveDp(
+        compact: Dp,
+        medium: Dp,
+        expanded: Dp,
+        windowSizeClass: WindowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    ): Dp {
+        val windowWidthSizeClass = windowSizeClass.windowWidthSizeClass
+        val windowHeightSizeClass = windowSizeClass.windowHeightSizeClass
+
+        return when {
+            windowWidthSizeClass.isCompact || windowHeightSizeClass.isCompact ->  compact
+            windowWidthSizeClass.isMedium -> medium
+            windowWidthSizeClass.isExpanded -> expanded
+            else -> compact
+        }
+    }
+
+
+    /**
+     * A function to get an adaptive TextUnit size unit based on the window size class.
+     *
+     * @param windowSizeClass The current window size class.
+     * @param compact The TextUnit size for a compact window size. Also is the default value if no window size class is provided.
+     * @param medium The TextUnit size for a medium window size.
+     * @param expanded The TextUnit size for an expanded window size.
+     *
+     * @return The DP size for the current window size class.
+     * */
+    @Composable
+    fun getAdaptiveTextUnit(
+        compact: TextUnit,
+        medium: TextUnit,
+        expanded: TextUnit,
+        windowSizeClass: WindowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    ): TextUnit {
+        val windowWidthSizeClass = windowSizeClass.windowWidthSizeClass
+        val windowHeightSizeClass = windowSizeClass.windowHeightSizeClass
+
+        return when {
+            windowWidthSizeClass.isCompact || windowHeightSizeClass.isCompact ->  compact
+            windowWidthSizeClass.isMedium -> medium
+            windowWidthSizeClass.isExpanded -> expanded
+            else -> compact
+        }
+    }
+
+    /**
+     * A function to get an adaptive DP size unit based on the window size class.
+     *
+     * @param dp The DP to get an adaptive size for.
+     * @param incrementedDp The DP to increment the size by per window size class.
+     *
+     * @return The DP size for the current window size class.
+     * */
+    @Composable
+    fun getAdaptiveDp(
+        dp: Dp,
+        incrementedDp: Dp = 4.dp,
+    ): Dp {
+        return getAdaptiveDp(
+            compact = dp,
+            medium = dp + incrementedDp,
+            expanded = dp + (incrementedDp * 2)
+        )
+    }
+
+    /**
+     * A function to get an adaptive TextUnit size unit based on the window size class.
+     *
+     * @param size The TextUnit to get an adaptive size for.
+     * @param incrementedValue The DP to increment the size by per window size class.
+     *
+     * @return The DP size for the current window size class.
+     * */
+    @Composable
+    fun getAdaptiveTextUnit(
+        size: TextUnit,
+        incrementedValue: Int = 4,
+    ): TextUnit {
+        return getAdaptiveTextUnit(
+            compact = size,
+            medium = TextUnit(
+                value = size.value + incrementedValue.toFloat(),
+                type = size.type
+            ),
+            expanded = TextUnit(
+                value = size.value + (incrementedValue * 2).toFloat(),
+                type = size.type
+            )
+        )
+    }
+}
