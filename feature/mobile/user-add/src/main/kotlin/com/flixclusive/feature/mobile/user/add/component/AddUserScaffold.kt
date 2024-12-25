6a46896ef587fdf7980e24fb3cb12aad8b4cc8aa
rhenwinch <rhenwinchester@gmail.com>
2024-12-25 23:24:47 +0800

    fix(user-add-ui): add navigation bar padding

--- feature/mobile/user-add/src/main/kotlin/com/flixclusive/feature/mobile/user/add/component/AddUserScaffold.kt
+++ feature/mobile/user-add/src/main/kotlin/com/flixclusive/feature/mobile/user/add/component/AddUserScaffold.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -38,7 +38,9 @@ internal fun AddUserScaffold(
 ) {
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
     ) {
         content()
 
@@ -58,7 +60,6 @@ private fun FloatingBackButton(
 ) {
     Box(
         modifier = modifier
-            .statusBarsPadding()
             .padding(vertical = getAdaptiveDp(6.dp))
             .clip(MaterialTheme.shapes.small)
             .clickable {
