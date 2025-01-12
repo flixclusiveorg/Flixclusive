df38e11b6f5eab42eecb9f95ec01b289a097e3c1
rhenwinch <rhenwinchester@gmail.com>
2025-01-12 19:44:10 +0800

    fix(datastore): make `currentUser` get latest data from Room

--- domain/user/src/main/kotlin/com/flixclusive/domain/user/DefaultUserSessionManager.kt
+++ domain/user/src/main/kotlin/com/flixclusive/domain/user/DefaultUserSessionManager.kt
@@ -1,53 +1,64 @@
 package com.flixclusive.domain.user
 
 import com.flixclusive.core.datastore.UserSessionDataStore
+import com.flixclusive.core.util.coroutines.AppDispatchers
 import com.flixclusive.core.util.coroutines.AppDispatchers.Companion.withIOContext
 import com.flixclusive.data.user.UserRepository
 import com.flixclusive.model.database.User
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DefaultUserSessionManager @Inject constructor(
-    private val userRepository: UserRepository,
-    private val dataStoreManager: UserSessionDataStore
-) : UserSessionManager {
-    private val _currentUser = MutableStateFlow<User?>(null)
-    override val currentUser: StateFlow<User?> = _currentUser.asStateFlow()
+class DefaultUserSessionManager
+    @Inject
+    constructor(
+        private val userRepository: UserRepository,
+        private val userSessionDataStore: UserSessionDataStore,
+    ) : UserSessionManager {
+        override val currentUser: StateFlow<User?> =
+            userSessionDataStore.currentUserId
+                .filterNotNull()
+                .flatMapLatest { userRepository.observeUser(it) }
+                .stateIn(
+                    scope = AppDispatchers.Default.scope,
+                    started = SharingStarted.WhileSubscribed(5000),
+                    initialValue = null,
+                )
 
-    override suspend fun restoreSession() {
-        withIOContext {
-            // Attempt to restore user from last saved session
-            val savedUserId = dataStoreManager.currentUserId.first()
-            val sessionTimeout = dataStoreManager.sessionTimeout.first()
+        override suspend fun restoreSession() {
+            withIOContext {
+                // Attempt to restore user from last saved session
+                val savedUserId = userSessionDataStore.currentUserId.first()
+                val sessionTimeout = userSessionDataStore.sessionTimeout.first()
 
-            if (sessionTimeout < System.currentTimeMillis()) {
-                return@withIOContext signOut()
+                if (sessionTimeout < System.currentTimeMillis()) {
+                    return@withIOContext signOut()
+                }
+
+                savedUserId?.let { userId ->
+                    userRepository
+                        .getUser(id = userId)
+                        ?.let { signIn(it) }
+                        ?: signOut()
+                }
             }
+        }
 
-            savedUserId?.let { userId ->
-                userRepository.getUser(id = userId)
-                    ?.let { signIn(it) }
-                    ?: signOut()
+        override suspend fun signIn(user: User) {
+            withIOContext {
+                userSessionDataStore.saveCurrentUserId(user.id)
+            }
+        }
+
+        override suspend fun signOut() {
+            withIOContext {
+                userSessionDataStore.clearCurrentUser()
             }
         }
     }
-
-    override suspend fun signIn(user: User) {
-        withIOContext {
-            dataStoreManager.saveCurrentUserId(user.id)
-            _currentUser.value = user
-        }
-    }
-
-    override suspend fun signOut() {
-        withIOContext {
-            dataStoreManager.clearCurrentUser()
-            _currentUser.value = null
-        }
-    }
-}
