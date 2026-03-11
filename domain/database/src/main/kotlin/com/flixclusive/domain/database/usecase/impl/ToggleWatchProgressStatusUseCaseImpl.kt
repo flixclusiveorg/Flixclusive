69cd15ab03bc14a3eda1defc2ca828c2cd11c1dd
rhenwinch <rhenwinchester@gmail.com>
2026-03-12 00:31:11 +0800

    fix(film): improve `mark as watched` toggle use case

--- domain/database/src/main/kotlin/com/flixclusive/domain/database/usecase/impl/ToggleWatchProgressStatusUseCaseImpl.kt
+++ domain/database/src/main/kotlin/com/flixclusive/domain/database/usecase/impl/ToggleWatchProgressStatusUseCaseImpl.kt
@@ -2,7 +2,6 @@ package com.flixclusive.domain.database.usecase.impl
 
 import com.flixclusive.core.database.entity.watched.EpisodeProgress
 import com.flixclusive.core.database.entity.watched.MovieProgress
-import com.flixclusive.core.database.entity.watched.WatchProgress
 import com.flixclusive.core.database.entity.watched.WatchStatus
 import com.flixclusive.data.database.repository.WatchProgressRepository
 import com.flixclusive.data.database.session.UserSessionManager
@@ -13,75 +12,82 @@ import com.flixclusive.model.film.TvShow
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
-internal class ToggleWatchProgressStatusUseCaseImpl
-    @Inject
-    constructor(
-        private val watchProgressRepository: WatchProgressRepository,
-        private val userSessionManager: UserSessionManager,
-    ) : ToggleWatchProgressStatusUseCase {
-        override suspend fun invoke(film: Film) {
-            val ownerId = userSessionManager.currentUser.first()?.id
-            requireNotNull(ownerId) {
-                "User must be logged in to toggle watch progress"
-            }
-
-            val progress = watchProgressRepository
-                .getAsFlow(
-                    id = film.identifier,
-                    ownerId = ownerId,
-                    type = film.filmType,
-                ).first()
-
-            when (progress) {
-                null -> {
-                    watchProgressRepository.insert(
-                        item = createWatchProgress(ownerId, film),
-                        film = film,
-                    )
-                }
-
-                else -> {
-                    watchProgressRepository.delete(
-                        item = progress.id,
-                        type = film.filmType,
-                    )
-                }
-            }
+internal class ToggleWatchProgressStatusUseCaseImpl @Inject constructor(
+    private val watchProgressRepository: WatchProgressRepository,
+    private val userSessionManager: UserSessionManager,
+) : ToggleWatchProgressStatusUseCase {
+    override suspend fun invoke(film: Film) {
+        val ownerId = userSessionManager.currentUser.first()?.id
+        requireNotNull(ownerId) {
+            "User must be logged in to toggle watch progress"
         }
 
-        /**
-         * Creates a new [WatchProgress] instance for the given [film] and the current user.
-         *
-         * @param ownerId The ID of the current user.
-         * @param film The film for which to create the watch progress.
-         *
-         * @return A new [WatchProgress] instance with status set to [WatchStatus.COMPLETED].
-         *
-         * @throws IllegalArgumentException if the [film] is not of type [Movie] or [TvShow].
-         * */
-        private fun createWatchProgress(
-            ownerId: Int,
-            film: Film,
-        ): WatchProgress {
-            return if (film is Movie) {
-                MovieProgress(
-                    filmId = film.identifier,
-                    ownerId = ownerId,
-                    progress = 0L,
-                    status = WatchStatus.COMPLETED,
-                )
-            } else if (film is TvShow) {
-                // To avoid costly lookups, just set the total episodes as the last watched episode
-                EpisodeProgress(
+        when (film) {
+            is Movie -> invokeForMovie(ownerId, film)
+            is TvShow -> invokeForTvShow(ownerId, film)
+        }
+    }
+
+    private suspend fun invokeForTvShow(ownerId: Int, film: TvShow) {
+        val progress = watchProgressRepository.get(
+            id = film.identifier,
+            ownerId = ownerId,
+            type = film.filmType,
+        )
+
+        if (progress == null || (progress.watchData as EpisodeProgress).episodeNumber < film.totalEpisodes) {
+            watchProgressRepository.insert(
+                film = film,
+                item = EpisodeProgress(
                     filmId = film.identifier,
                     ownerId = ownerId,
                     seasonNumber = film.totalSeasons,
                     episodeNumber = film.totalEpisodes,
                     progress = 0L,
                     status = WatchStatus.COMPLETED,
+                ),
+            )
+        } else {
+            for (i in 1..film.totalSeasons) {
+                val progressList = watchProgressRepository.getSeasonProgress(
+                    tvShowId = film.identifier,
+                    ownerId = ownerId,
+                    seasonNumber = i,
                 )
-            } else {
-                throw IllegalArgumentException("Unsupported FilmMetadata type for watch progress: $film")
+
+                progressList.forEach { episodeProgress ->
+                    watchProgressRepository.delete(
+                        item = episodeProgress.id,
+                        type = film.filmType,
+                    )
+                }
             }
+
         }
     }
+
+    private suspend fun invokeForMovie(ownerId: Int, film: Movie) {
+        val progress = watchProgressRepository.get(
+            id = film.identifier,
+            ownerId = ownerId,
+            type = film.filmType,
+        )
+
+        if (progress == null) {
+            watchProgressRepository.insert(
+                film = film,
+                item = MovieProgress(
+                    filmId = film.identifier,
+                    ownerId = ownerId,
+                    progress = 0L,
+                    status = WatchStatus.COMPLETED,
+                ),
+            )
+        } else {
+            watchProgressRepository.delete(
+                item = progress.id,
+                type = film.filmType,
+            )
+        }
+    }
+}
