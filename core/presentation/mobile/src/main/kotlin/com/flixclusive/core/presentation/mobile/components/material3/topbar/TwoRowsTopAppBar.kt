58ed58e72e45f1d075a0a92d18bafc8804fd5604
rhenwinch <rhenwinchester@gmail.com>
2026-05-05 02:41:13 +0800

    fix(library): resolve critical bug due to topbar measurement

--- core/presentation/mobile/src/main/kotlin/com/flixclusive/core/presentation/mobile/components/material3/topbar/TwoRowsTopAppBar.kt
+++ core/presentation/mobile/src/main/kotlin/com/flixclusive/core/presentation/mobile/components/material3/topbar/TwoRowsTopAppBar.kt
@@ -147,13 +147,11 @@ fun TwoRowsTopAppBar(
                         .clipToBounds()
                         .heightIn(max = collapsedHeight),
                 scrolledOffset = {
+                    val bottom = bottomContentHeightPx.floatValue
                     val offset = scrollBehavior?.state?.heightOffset ?: 0f
-
-                    if (offset >= -bottomContentHeightPx.floatValue) {
-                        0f
-                    } else {
-                        offset.plus(bottomContentHeightPx.floatValue)
-                    }
+                    if (bottom == 0f) 0f
+                    else if (offset >= -bottom) 0f
+                    else offset + bottom
                 },
                 navigationIconContentColor = colors.navigationIconContentColor,
                 titleContentColor = colors.titleContentColor,
